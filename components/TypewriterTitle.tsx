fa445920f2c0802b1630f85289eabb5c02fff599
gedxxe <gedebagus@students.unnes.ac.id>
2025-06-03 01:42:18 +0700

    Refresh UI Model

--- components/TypewriterTitle.tsx
+++ components/TypewriterTitle.tsx
@@ -6,7 +6,7 @@ const TypewriterTitle: React.FC = () => {
   const typedTitle = useTypewriter(APP_TITLE, 75);
 
   return (
-    <h1 className="text-4xl md:text-5xl font-mono font-bold text-center text-slate-100 mb-10 tracking-wider">
+    <h1 className="text-2xl md:text-3xl font-mono font-bold text-center text-slate-100 mb-10 tracking-wider">
       {typedTitle}
       <span className="caret-blink">_</span>
     </h1>
