ad117dfb8d4ffdd6fb3378e9def088db013329e2
gedxxe <gedebagus@students.unnes.ac.id>
2025-06-11 23:44:07 +0700

    Revision for major typos

--- constants.ts
+++ constants.ts
@@ -7,7 +7,7 @@ export const MODEL_CONFIGS: ModelConfig[] = [
   {
     id: 'cnn',
     name: 'CNN Model',
-    path: './model_cnn.onnx', // Ensure these models are in the same directory as index.html (e.g., project root for Vercel deployment)
+    path: './model_cnn.onnx',
     inputShape: [1, 150, 150, 1], // NHWC
     isGrayscale: true,
     targetSize: { width: 150, height: 150 },
@@ -15,10 +15,42 @@ export const MODEL_CONFIGS: ModelConfig[] = [
   {
     id: 'mobilenet',
     name: 'MobileNetV2 Model',
-    path: './model_mobilenet.onnx', // Ensure these models are in the same directory as index.html (e.g., project root for Vercel deployment)
+    path: './model_mobilenet.onnx',
     inputShape: [1, 224, 224, 3], // NHWC
     isGrayscale: false,
     targetSize: { width: 224, height: 224 },
+  },
+  {
+    id: 'densenet169',
+    name: 'DenseNet169 Model',
+    path: './model_densenet169.onnx',
+    inputShape: [1, 224, 224, 3], // NHWC - Assumed
+    isGrayscale: false,             // Assumed
+    targetSize: { width: 224, height: 224 }, // Assumed
+  },
+  {
+    id: 'mobilenetv3',
+    name: 'MobileNetV3 Model',
+    path: './model_mobilenetv3.onnx',
+    inputShape: [1, 224, 224, 3], // NHWC - Assumed
+    isGrayscale: false,             // Assumed
+    targetSize: { width: 224, height: 224 }, // Assumed
+  },
+  {
+    id: 'resnet152',
+    name: 'ResNet152 Model',
+    path: './model_resnet152.onnx',
+    inputShape: [1, 224, 224, 3], // NHWC - Assumed
+    isGrayscale: false,             // Assumed
+    targetSize: { width: 224, height: 224 }, // Assumed
+  },
+  {
+    id: 'vgg19',
+    name: 'VGG19 Model',
+    path: './model_vgg19.onnx',
+    inputShape: [1, 224, 224, 3], // NHWC - Assumed
+    isGrayscale: false,             // Assumed
+    targetSize: { width: 224, height: 224 }, // Assumed
   },
 ];
 
